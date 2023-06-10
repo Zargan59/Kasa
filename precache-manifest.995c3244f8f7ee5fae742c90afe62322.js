f9f8c59b6e0dfe2ee951028eebe71b74305fccaf
Tristan Destoop <tristan.Destoop@gmail.com>
2023-06-10 16:35:10 +0200

    Updates

---
+++ precache-manifest.995c3244f8f7ee5fae742c90afe62322.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "df307a315c127103f823",
+    "url": "/Kasa/static/css/main.42d208eb.chunk.css"
+  },
+  {
+    "revision": "df307a315c127103f823",
+    "url": "/Kasa/static/js/main.df307a31.chunk.js"
+  },
+  {
+    "revision": "c4cf4b3db41e12fdc6b9",
+    "url": "/Kasa/static/js/1.c4cf4b3d.chunk.js"
+  },
+  {
+    "revision": "f96be834ff606df2e576",
+    "url": "/Kasa/static/js/runtime~main.f96be834.js"
+  },
+  {
+    "revision": "e57d824b9fcbbe3457fe13dcedd1b866",
+    "url": "/Kasa/static/media/Banner_Home.e57d824b.png"
+  },
+  {
+    "revision": "666ea1151b409cda2d4861756fb08a4f",
+    "url": "/Kasa/static/media/Banner_About.666ea115.png"
+  },
+  {
+    "revision": "3bab9c2fec2c765c464963c5c706e55c",
+    "url": "/Kasa/index.html"
+  }
+];
